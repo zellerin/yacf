c47312f1137fe30f7c89b0a2b5f18f240ed2aae0
Tomas Zellerin <zellerin@gmail.com>
2015-02-12 20:09:48 +0000

    One more page.
    Now [  and ] works, shared function compile-list.

--- source.lisp
+++ source.lisp
@@ -18,11 +18,11 @@
 
 (defvar letter-codes
   (vector
-   #x00 #xfa #x00 #x00 #x00 #x00 #x00 #x00 #x00 #x00 #xfd #xfb #xfe #xf3 #xf5 #xf7 
-   #xf4 #xf2 #xf6 #xeb #xec #xed #xee #xef #xf0 #xf1 #xf9 #xf8 #x00 #x00 #x00 #xff 
-   #xfc #x05 #xe3 #x52 #xe0 #x04 #x56 #x55 #xe4 #x07 #xea #xe8 #x54 #x51 #x06 #x03 
-   #xe2 #xe7 #x01 #x50 #x02 #xe6 #xe1 #x57 #xe5 #x53 #xe9 #x00 #x00 #x00 #x00 #x00 
-   #x00 #x05 #xe3 #x52 #xe0 #x04 #x56 #x55 #xe4 #x07 #xea #xe8 #x54 #x51 #x06 #x03 
+   #x00 #xfa #x00 #x00 #x00 #x00 #x00 #x00 #x00 #x00 #xfd #xfb #xfe #xf3 #xf5 #xf7
+   #xf4 #xf2 #xf6 #xeb #xec #xed #xee #xef #xf0 #xf1 #xf9 #xf8 #x00 #x00 #x00 #xff
+   #xfc #x05 #xe3 #x52 #xe0 #x04 #x56 #x55 #xe4 #x07 #xea #xe8 #x54 #x51 #x06 #x03
+   #xe2 #xe7 #x01 #x50 #x02 #xe6 #xe1 #x57 #xe5 #x53 #xe9 #x00 #x00 #x00 #x00 #x00
+   #x00 #x05 #xe3 #x52 #xe0 #x04 #x56 #x55 #xe4 #x07 #xea #xe8 #x54 #x51 #x06 #x03
    #xe2 #xe7 #x01 #x50 #x02 #xe6 #xe1 #x57 #xe5 #x53 #xe9 #x00 #x00 #x00 #x00 #x00)
   "Translation table for codes"
 )
@@ -47,39 +47,45 @@
 (defun compile-comment (output symbol)
   (encode-word output symbol 5))
 
-(defun compile-symbol (output symbol)
-  (encode-word output symbol 7))
+(defun compile-list (output default-symbol list)
+  (loop for i in list
+     do (etypecase i
+	  ((eql cr) (encode-word output i
+				 (if (= default-symbol 2)
+				     default-symbol 4)))
+	  ((eql [) (setq default-symbol 7))
+	  ((eql ]) (setq default-symbol 2))
+	  ((integer * -1) (compile-number output i 1))
+	  ((integer 0) (compile-number output i))
+	  (symbol (encode-word output i default-symbol))
+	  (cons (compile-cons output i)))))
 
 (defun fe::defun (output word &rest code)
   (encode-word output word 3)
-  (loop for i in code
-     do (etypecase i
-	  ((integer * -1) (compile-number output i 1))
-	  ((integer 0) (compile-number output i))
-	  (symbol (encode-word output i 2))
-	  (cons (compile-cons output i))))
+  (compile-list output 2 code)
   (encode-word output '|;| 2))
+
+(defun fe::def-top-op (output word code &optional asm)
+  "Define word that makes an 2byte operation followed by number"
+  (declare (ignorable asm))
+  (fe::defun output word code '|2c,n|))
 
 (defun compile-cons (output item)
   (case (car item)
     (dec (compile-number output (second item) 1))
     (cmt (mapcar (lambda (a) (compile-comment output a))
 		 (cdr item)))
-    (t (apply (find-symbol (symbol-name (car item)) 'fe)
-		output (cdr item)))))
+    (t (apply (or
+	       (find-symbol (symbol-name (car item)) 'fe)
+	       (error "No fe function ~s" (car item)))
+	      output (cdr item)))))
 
 (defmacro with-page (name options &body body)
   (declare (ignorable options name))
   `(let ((output (make-array 512
 			     :element-type 'word
 			     :fill-pointer 0)))
-     (loop for item in ',body
-	do (etypecase item
-	     ((eql cr) (encode-word output item 4))
-	     ((integer * -1) (compile-number output item 1))
-	     ((integer 0) (compile-number output item))
-	     (symbol (compile-symbol output item))
-	     (cons (compile-cons output item))))
+     (compile-list output 7 ',body)
      (setf (fill-pointer output) 128)
      output))
 
@@ -109,7 +115,7 @@
    cr 5 ld (cmt constants)
    cr 6 ld (cmt boot)
    cr (dec 0) bye)
-  
+
   (nrmacros empty (cmt x86 boot)
    (defun +s #xc083 |2c,n|)
    (defun + #x05 |c,,|)
@@ -117,13 +123,13 @@
    (defun @+ #x0503 |dc,s| |c,,|)
    (defun @-+ #x052b |dc,s| |c,,|)
    (defun @ |,put| -4 |,+stack| #xa1 |c,,|)
-   (defun ash #xf8c1 |2c,n|)
-   (defun lsr #xe8c1 |2c,n|)
-   (defun shl #xe0c1 |2c,n|)
-   (defun and #xe083 |2c,n|)
+   (def-top-op ash #xf8c1)
+   (def-top-op lsr #xe8c1)
+   (def-top-op shl #xe0c1)
+   (def-top-op and #xe083)
    (defun / #xbed231 |3c,| |,| #xf6f7 |2c,|)
    (defun cmp #x3d |c,,|)
-   (defun nth #x08438b |2c,n|)
+   (def-top-op nth #x08438b)
    (defun nop |,lit|)
    (defun reg! (dec 11) shl #x038b + |2c,|)
    (defun ldreg (dec 11) shl #xc089 + |2c,|)
@@ -150,5 +156,14 @@
    (defun da@+ #x78b |2c,| #x47f8d |3c,|)
    (defun da! #xc789 |2c,|) (cmt dup a!)
    (defun tocl #xc189 |2c,|)
-   |;s|))
+   |;s|)
 
+  (forth (cmt x86 boot )
+	 (defun - - )
+	 (defun eax (dec 0) ) (defun ecx (dec 1) ) (defun edx (dec 2)) (defun ebx (dec 3))
+	 (defun esp (dec 4) ) (defun ebp (dec 5) ) (defun esi (dec 6) ) (defun edi (dec 7))
+	 (defun r. [ edx ] pop [ eax ] pop [ edx ] push)
+	 (defun dropdup #x038b |2c,|)
+	 (defun break break)
+	 |;s|)
+  )
